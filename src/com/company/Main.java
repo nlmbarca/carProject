4506702f5b05c19330d1f91bedd96dcb0ff34698
nlmbarca <nicoortuno13@gmail.com>
2021-11-03 20:55:13 -0400

    the damn car you asked for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Car newCar=new Car();
-        newCar.name="Ferarri";
-        newCar.color="blue";
-        newCar.door=3;
-        newCar.engineCapacity="60000cc";
-        
-        System.out.println(newCar.color);
+
+        newCar.name="Lambo urus";
+        newCar.engineCapacity="";
+        newCar.door=7;
+        newCar.model="urus";
+
+        System.out.println(newCar.model);
+
     }
 }
